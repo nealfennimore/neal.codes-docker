cec09cf0b35cd07928e0663f25917b5f8953034f
nealfennimore <hi@neal.codes>
2016-08-26 12:58:06 -0700

    Update app_cmd to ensure access to npm. Add in express execution

--- docker/containers/app/app_cmd.sh
+++ docker/containers/app/app_cmd.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+# Reload NVM
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" || exit 1
+
 cd $ROOT_DIR
-exec npm run start
+npm rebuild node-sass
+npm run build
+
+exec node $ROOT_DIR/server/express.js
