3f490637d961e5988c3e614b37217e9791f00f9f
nealfennimore <hi@neal.codes>
2016-09-18 14:00:55 -0700

    Update shell scripts to exclude env file types based on docker.env file environment

--- utils.sh
+++ utils.sh
@@ -1,13 +1,41 @@
 #!/usr/bin/env bash
 
-removeComments (){
+ENVIRONMENTS=(test development production)
+NON_ENV=()
+
+function removeComments (){
     pcregrep -Mv '#[\w\s]*$' $@;
 }
 
-getEnvVars (){
-    find docker -name '*.env' |
+function getEnv (){
+    cat docker/docker.env | removeComments | grep ENVIRONMENT | sed 's/ENVIRONMENT=//';
+}
+
+function getNonEnv (){
+    local COUNT=`echo ${#NON_ENV[@]}`
+    if [[ $COUNT > 0 ]]; then
+        return
+    fi
+
+    local CURRENT_ENV=$(getEnv)
+    for i in ${ENVIRONMENTS[@]}; do
+        if [[ $i != $CURRENT_ENV ]]; then
+            NON_ENV+=($i)
+        fi
+    done
+}
+
+function getEnvVars (){
+    find docker -name '*.env' $(getExcludedFiles) |
     sort -r | #Make sure development files load after production files   so vars overwrite
     while read i; do echo "$(cat $i)"; done |
     removeComments |
     xargs;
+}
+
+function getExcludedFiles {
+    getNonEnv
+    for i in ${NON_ENV[@]}; do
+        printf "! -name *${i}* "
+    done
 }
