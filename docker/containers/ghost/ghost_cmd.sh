25d5088a57799a277f8336d1865e7a16e04e5777
nealfennimore <hi@neal.codes>
2016-09-20 22:21:22 -0700

    Add in initial ghost containers. Update compose

---
+++ docker/containers/ghost/ghost_cmd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Reload NVM
+[ -s "$GHOST_NVM_DIR/nvm.sh" ] && . "$GHOST_NVM_DIR/nvm.sh" || exit 1
+
+CURRENT_ENVIRONMENT=$(printenv ENVIRONMENT)
+
+npm rebuild # For different environments
+git submodule update --init --recursive
+npm install -g grunt-cli
+npm install
+grunt init
+
+if [ $CURRENT_ENVIRONMENT == "development" ]; then
+    exec npm start
+else
+    grunt prod
+    exec npm start --production
+fi
