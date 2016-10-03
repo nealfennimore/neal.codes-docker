010a4acef14854b68a0eec0e9bf35f905d0aaf36
nealfennimore <hi@neal.codes>
2016-10-02 20:50:01 -0700

    Update ghost url

--- docker/containers/ghost/config.js
+++ docker/containers/ghost/config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://localhost/blog',
+        url: 'http://$HOST_NAME/blog',
         mail: {},
         database: {
             client: 'sqlite3',
@@ -31,7 +31,7 @@ config = {
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
         // Change this to your Ghost blog's published URL.
-        url: 'http://localhost/blog',
+        url: 'http://$HOST_NAME/blog',
 
         // Example refferer policy
         // Visit https://www.w3.org/TR/referrer-policy/ for instructions
