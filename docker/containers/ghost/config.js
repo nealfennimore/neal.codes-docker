845b25b65983a3d311d6b87b5e487916b2780e64
nealfennimore <hi@neal.codes>
2016-10-05 22:40:10 -0700

    Update ghost to act as api/admin

--- docker/containers/ghost/config.js
+++ docker/containers/ghost/config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://$HOST_NAME/blog',
+        url: 'http://$HOST_NAME/$GHOST_URL_PATH',
         mail: {},
         database: {
             client: 'sqlite3',
@@ -31,7 +31,7 @@ config = {
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
         // Change this to your Ghost blog's published URL.
-        url: 'http://$HOST_NAME/blog',
+        url: 'http://$HOST_NAME/$GHOST_URL_PATH',
 
         // Example refferer policy
         // Visit https://www.w3.org/TR/referrer-policy/ for instructions
