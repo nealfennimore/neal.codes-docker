a40ce0f1d00f3614d94f98b52536b483f9cb8567
Neal Fennimore <hi@neal.codes>
2016-11-01 17:38:56 -0700

    Update ghost cmd

--- docker/containers/ghost/ghost_cmd.sh
+++ docker/containers/ghost/ghost_cmd.sh
@@ -6,14 +6,17 @@
 CURRENT_ENVIRONMENT=$(printenv ENVIRONMENT)
 
 npm rebuild # For different environments
+
 git submodule update --init --recursive
+
+npm install -g grunt-cli knex-migrator
 npm install
+
 grunt init
 
 if [ $CURRENT_ENVIRONMENT == "development" ]; then
     exec npm start
 else
-    npm install -g grunt-cli
     grunt prod
     exec npm start --production
 fi
