3f13858d911de98182af8314df8c2045be5ba3d0
nealfennimore <hi@neal.codes>
2016-11-15 10:16:55 -0800

    Add app config to docker dirs

---
+++ docker/containers/app/config.js
@@ -0,0 +1,27 @@
+import path from 'path';
+import buildVersion from './buildStamp.js';
+
+const config = {
+    server: {
+        port: $APP_PORT,
+        devPort: $APP_DEV_PORT,
+        ip: '0.0.0.0',
+        hostname: '$HOST_NAME',
+        protocol: 'https'
+    },
+    paths: {
+        CLIENT: path.resolve(__dirname, 'client'),
+        SERVER: path.resolve(__dirname, 'server'),
+        PUBLIC: path.resolve(__dirname, 'public'),
+        DEV: path.resolve(__dirname, 'dev')
+    },
+    regex: {
+        VENDOR_FILES: /vendor(Styles)?\.(scss|css|js)$/,
+        VENDOR_SCSS: /vendorStyles\.scss$/
+    },
+    webpack: {
+        cssModuleName: `[name]-[local]__${buildVersion}`
+    }
+};
+
+export default config;
