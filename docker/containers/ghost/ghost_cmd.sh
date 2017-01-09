675372a0eb1a1b7ac193f08e8a683e5debe882d8
Neal Fennimore <hi@neal.codes>
2017-01-08 20:04:22 -0800

    Update ghost build steps to work

--- docker/containers/ghost/ghost_cmd.sh
+++ docker/containers/ghost/ghost_cmd.sh
@@ -7,7 +7,7 @@ CURRENT_ENVIRONMENT=$(printenv ENVIRONMENT)
 
 npm rebuild # For different environments
 
-git submodule update --init --recursive
+rm .git && git init && git submodule update --init --recursive # reset submodule root path
 
 npm install -g grunt-cli knex-migrator
 npm install
