eed594d032370338e5c11205cb70abe3e401829e
Neal Fennimore <hi@neal.codes>
2017-01-09 18:06:42 -0800

    Update ghost config to symbolically link .git dir

--- docker/containers/ghost/ghost_cmd.sh
+++ docker/containers/ghost/ghost_cmd.sh
@@ -8,7 +8,13 @@ CURRENT_ENVIRONMENT=$(printenv ENVIRONMENT)
 sudo chown -R ghost:ghost $GHOST_USER_DIR
 
 npm rebuild # For different environments
-rm -rf .git && git init && git submodule update --init --recursive # reset submodule root path
+
+if [! -d $DOCKER_ROOT_DIR ]; then
+    sudo mkdir -p $DOCKER_ROOT_DIR
+fi
+
+sudo ln -s $GHOST_ROOT_DIR/.git $DOCKER_ROOT_DIR/.git
+# rm -rf .git && git init && git submodule update --init --recursive # reset submodule root path
 
 npm install -g grunt-cli knex-migrator
 npm install
