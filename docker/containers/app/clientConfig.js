58b4e64a2a2f213a5b7343d239a3a77811559146
Neal Fennimore <hi@neal.codes>
2017-01-19 22:42:09 -0800

    Add in disqus and google analytics. Add in client config

---
+++ docker/containers/app/clientConfig.js
@@ -0,0 +1,10 @@
+const clientConfig = {
+    google: {
+        analytics: '$APP_GOOGLE_ANALYTICS_ID'
+    },
+    disqus: {
+        shortname: '$APP_DISQUS_SHORTNAME'
+    }
+};
+
+export default clientConfig;
