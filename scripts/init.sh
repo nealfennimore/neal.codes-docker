86b7d9d3a4f2efea6dcf1c76b9a32f05b112f7ab
Neal Fennimore <hi@neal.codes>
2017-03-23 21:32:59 -0700

    Update init script to not be run with sudo. Update to get repo push ready

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,8 +3,11 @@
 DIR="$(pwd -P)"
 SCRIPT_DIR="$DIR/scripts"
 
-sudo ln -sf scripts/utils.sh utils.sh
-sudo ln -sf scripts/build.sh build.sh
-sudo ln -sf scripts/start.sh start.sh
+ln -sf scripts/utils.sh utils.sh
+ln -sf scripts/build.sh build.sh
+ln -sf scripts/start.sh start.sh
 
-sudo ln -sf $SCRIPT_DIR/git-post-update.sh .git/hooks/post-update
+ln -sf $SCRIPT_DIR/git-post-update.sh .git/hooks/post-update
+
+# Allow git directory to be pushed to
+git config receive.denyCurrentBranch updateInstead
